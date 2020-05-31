e5b78dab698072f4de5ea0d57999d992d1bee4ad
chloestephan <chloe.stephan@efrei.net>
2020-05-31 20:00:40 +0200

    Debut a vous de jouer

--- js/script.js
+++ js/script.js
@@ -1,28 +1,80 @@
-/* Toggle between adding and removing the "responsive" class to top-navigation when the user clicks on the icon */
-function responsive_top() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "top-navigation") {
-        x.className += " responsive_top";
-    } else {
-        x.className = "top-navigation";
-    }
+
+/* changes the background color of the main part of the "your turn" page*/
+var color =["#ff0000", "#ffa500", "#ffff00", "#90ee90", "#008000", "#40e0d0","#0000ff","#dda0dd","#ffb6c1" ,"#ffffff"];
+var i = 0;
+document.querySelector("button").addEventListener("click", function(){
+  i = 1 < color.length ? ++i : 0;
+  document.querySelector("main").style.background = color[i]
+})
+
+/* make element dragable */
+// Make the DIV element draggable:
+dragElement(document.getElementById("moving-div"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
-function responsive_bottom() {
-    var x = document.getElementById("myBotbar");
-    if (x.className === "bottom-navigation") {
-        x.className += " responsive_bottom";
-    } else {
-        x.className = "bottom-navigation";
-    }
+/* guessing game */
+// random value generated 
+var y = Math.floor(Math.random() * 10 + 1); 
+      
+// counting the number of guesses 
+var guess = 1; 
+  
+document.getElementById("submitguess").onclick = function(){     
+  // number guessed by user      
+  var x = document.getElementById("guessField").value; 
+
+  if(x == y) 
+  {     
+    alert("Congratulations! You guessed it right in" + guess + " guesses."); 
+  } 
+  else if(x > y) /* if guessed number is greater than actual number*/ 
+  {     
+    guess++; 
+    alert("Here's a tip: try a smaller number."); 
+  } 
+  else
+  { 
+    guess++; 
+    alert("Oops! Try a bigger number") 
+  } 
 }
-
-  $("#content a[href^='http://']").attr("target","_blank");
-
-$(function(){
-  var includes = $('[data-include]');
-  jQuery.each(includes, function(){
-    var file = $(this).data('include') + '.html';
-    $(this).load(file);
-  });
-});
